56afb3d345100e327affca147fdf0cc9de82f9ab
jokovivanco <jokovivanco@gmail.com>
2025-08-12 20:17:11 +0800

    small change

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,6 +6,7 @@ export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <>
+      <h1>Long live Turborepo!</h1>
       <CreateProduct />
       <Products />
     </>
